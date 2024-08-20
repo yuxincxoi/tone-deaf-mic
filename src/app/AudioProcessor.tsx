d77156385e9b07e89b1cb710c72663e921b8a1dc
최유진 <yjyj0774@gmail.com>
2024-08-20 16:51:48 +0900

    BiquadFilterNode 생성

--- src/app/AudioProcessor.tsx
+++ src/app/AudioProcessor.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const AudioProcessor = () => {
   const audioContextRef = useRef<AudioContext | null>(null);
   const microphoneRef = useRef<MediaStreamAudioSourceNode | null>(null);
+  const filterNodeRef = useRef<BiquadFilterNode | null>(null);
+  const [frequency, setFrequecy] = useState(1000);
 
   // 오디오 처리 초기화
   const initAudio = async () => {
@@ -13,6 +15,11 @@ const AudioProcessor = () => {
 
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true }); // 마이크 스트림 얻기
     microphoneRef.current = audioContext.createMediaStreamSource(stream); // 얻은 스트림을 오디오 컨텍스트에 전달
+
+    const filterNode = audioContext.createBiquadFilter(); // BiquadFilterNode 생성
+    filterNode.type = "lowpass"; // 필터 유형 설정 - 저역 필터
+    filterNode.frequency.setValueAtTime(frequency, audioContext.currentTime); // 초기 필터 주파수 설정
+    filterNodeRef.current = filterNode; // useRef에 저장
   };
 
   initAudio();
