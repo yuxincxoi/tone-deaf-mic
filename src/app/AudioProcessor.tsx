0dab789b4d4808e0c8bceb1c4dbd508497c05deb
최유진 <yjyj0774@gmail.com>
2024-08-20 12:40:13 +0900

    useRef에 오디오 컨텍스트 저장

--- src/app/AudioProcessor.tsx
+++ src/app/AudioProcessor.tsx
@@ -1,5 +1,10 @@
+import { useRef } from "react";
+
 const AudioProcessor = () => {
+  const audioContextRef = useRef<AudioContext | null>(null);
+
   const audioContext = new AudioContext(); // Web Audio API 기능에 접근하기 위해 인스턴스 생성
+  audioContextRef.current = audioContext as AudioContext; // useRef에 저장
 
   return (
     <>
