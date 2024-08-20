754e45aaebdfc6740dda7da39eba6e8185f280db
최유진 <yjyj0774@gmail.com>
2024-08-20 13:08:46 +0900

    오디오 처리 초기화 함수를 생성하여 비동기 작업 처리

--- src/app/AudioProcessor.tsx
+++ src/app/AudioProcessor.tsx
@@ -6,12 +6,16 @@ const AudioProcessor = () => {
   const audioContextRef = useRef<AudioContext | null>(null);
   const microphoneRef = useRef<MediaStreamAudioSourceNode | null>(null);
 
-  const audioContext = new AudioContext(); // Web Audio API 기능에 접근하기 위해 인스턴스 생성
-  audioContextRef.current = audioContext as AudioContext; // useRef에 저장
+  // 오디오 처리 초기화
+  const initAudio = async () => {
+    const audioContext = new AudioContext(); // Web Audio API 기능에 접근하기 위해 인스턴스 생성
+    audioContextRef.current = audioContext as AudioContext; // useRef에 저장
 
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true }); // 마이크 스트림 얻기
     microphoneRef.current = audioContext.createMediaStreamSource(stream); // 얻은 스트림을 오디오 컨텍스트에 전달
   };
+
+  initAudio();
 
   return (
     <>
