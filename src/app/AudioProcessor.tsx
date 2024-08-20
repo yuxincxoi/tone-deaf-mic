2e6224e30b2617b06f141e85c5a1221d33553f65
최유진 <yjyj0774@gmail.com>
2024-08-20 13:06:58 +0900

    얻은 마이크 스트림을 오디오 컨텍스트에 전달

--- src/app/AudioProcessor.tsx
+++ src/app/AudioProcessor.tsx
@@ -4,11 +4,14 @@ import { useRef } from "react";
 
 const AudioProcessor = () => {
   const audioContextRef = useRef<AudioContext | null>(null);
+  const microphoneRef = useRef<MediaStreamAudioSourceNode | null>(null);
 
   const audioContext = new AudioContext(); // Web Audio API 기능에 접근하기 위해 인스턴스 생성
   audioContextRef.current = audioContext as AudioContext; // useRef에 저장
 
-  const stream = navigator.mediaDevices.getUserMedia({ audio: true });
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true }); // 마이크 스트림 얻기
+    microphoneRef.current = audioContext.createMediaStreamSource(stream); // 얻은 스트림을 오디오 컨텍스트에 전달
+  };
 
   return (
     <>
