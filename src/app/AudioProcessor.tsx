d129a500304a54b77bcecca8c38001b7eb50854d
최유진 <yjyj0774@gmail.com>
2024-08-20 16:58:34 +0900

    isProcessing 또는 frequency가 변경되면 useEffect 실행

--- src/app/AudioProcessor.tsx
+++ src/app/AudioProcessor.tsx
@@ -1,31 +1,34 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const AudioProcessor = () => {
   const audioContextRef = useRef<AudioContext | null>(null);
   const microphoneRef = useRef<MediaStreamAudioSourceNode | null>(null);
   const filterNodeRef = useRef<BiquadFilterNode | null>(null);
   const [frequency, setFrequecy] = useState(1000);
+  const [isProcessing, setIsProcessing] = useState(false);
 
-  // 오디오 처리 초기화
-  const initAudio = async () => {
-    const audioContext = new AudioContext(); // Web Audio API 기능에 접근하기 위해 인스턴스 생성
-    audioContextRef.current = audioContext as AudioContext; // useRef에 저장
+  useEffect(() => {
+    // 오디오 처리 초기화
+    const initAudio = async () => {
+      const audioContext = new AudioContext(); // Web Audio API 기능에 접근하기 위해 인스턴스 생성
+      audioContextRef.current = audioContext as AudioContext; // useRef에 저장
 
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true }); // 마이크 스트림 얻기
-    microphoneRef.current = audioContext.createMediaStreamSource(stream); // 얻은 스트림을 오디오 컨텍스트에 전달
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true }); // 마이크 스트림 얻기
+      microphoneRef.current = audioContext.createMediaStreamSource(stream); // 얻은 스트림을 오디오 컨텍스트에 전달
 
-    const filterNode = audioContext.createBiquadFilter(); // BiquadFilterNode 생성
-    filterNode.type = "lowpass"; // 필터 유형 설정 - 저역 필터
-    filterNode.frequency.setValueAtTime(frequency, audioContext.currentTime); // 초기 필터 주파수 설정
-    filterNodeRef.current = filterNode; // useRef에 저장
+      const filterNode = audioContext.createBiquadFilter(); // BiquadFilterNode 생성
+      filterNode.type = "lowpass"; // 필터 유형 설정 - 저역 필터
+      filterNode.frequency.setValueAtTime(frequency, audioContext.currentTime); // 초기 필터 주파수 설정
+      filterNodeRef.current = filterNode; // useRef에 저장
 
-    microphoneRef.current.connect(filterNode); // 마이크 입력을 필터에 연결
-    filterNode.connect(audioContext.destination); // 필터를 오디오 출력에 연결
-  };
+      microphoneRef.current.connect(filterNode); // 마이크 입력을 필터에 연결
+      filterNode.connect(audioContext.destination); // 필터를 오디오 출력에 연결
+    };
 
-  initAudio();
+    initAudio();
+  }, [isProcessing, frequency]);
 
   return (
     <>
