0252723aa333a9f677113a024000c948987620aa
최유진 <yjyj0774@gmail.com>
2024-08-23 16:21:50 +0900

    audio context 상태관리

--- src/app/AudioProcessor.tsx
+++ src/app/AudioProcessor.tsx
@@ -3,21 +3,15 @@
 import { useEffect, useRef, useState } from "react";
 
 const AudioProcessor = () => {
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
   const audioContextRef = useRef<AudioContext | null>(null);
   const audioElementRef = useRef<HTMLAudioElement | null>(null);
 
   useEffect(() => {
     // 오디오 처리 초기화
     const initAudio = async () => {
-      const audioContext = new AudioContext(); // Web Audio API 기능에 접근하기 위해 인스턴스 생성
-      audioContextRef.current = audioContext as AudioContext; // useRef에 저장
-
-      if (audioElementRef.current) {
-        // 오디오 데이터를 오디오 노드로 변환
-        const sourceNode = audioContext.createMediaElementSource(
-          audioElementRef.current
-        );
-      }
+      const context = new AudioContext(); // Web Audio API 기능에 접근하기 위해 인스턴스 생성
+      setAudioContext(context);
     };
 
     initAudio();
