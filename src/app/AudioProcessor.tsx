35d579f80aebe704632321cfcfe76dca9caec91a
최유진 <yjyj0774@gmail.com>
2024-08-23 17:27:33 +0900

    왜곡 곡선 생성 및 적용

--- src/app/AudioProcessor.tsx
+++ src/app/AudioProcessor.tsx
@@ -37,6 +37,15 @@ const AudioProcessor = () => {
 
     const distortion = audioContext.createWaveShaper(); // 왜곡효과 적용하는 node 생성
     const gain = audioContext.createGain(); // 볼륨조절 node 생성
+
+    // 왜곡 곡선 생성
+    const curve = new Float32Array(44100);
+    for (let i = 0; i < 44100; i++) {
+      const x = (i * 2) / 44100 - 1;
+      curve[i] = (3 + x * 3) / (3 + Math.abs(x));
+    }
+    distortion.curve = curve; // 왜곡된 곡선을 입력신호에 설정
+    distortion.oversample = "4x"; // 왜곡 품질 향상
   };
   return (
     <>
