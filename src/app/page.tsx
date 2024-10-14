a5a7108ec515ed94199d4a94de3dbedacfa6f687
최유진 <yjyj0774@gmail.com>
2024-10-14 23:12:06 +0900

    korean title 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="w-96 m-auto pt-52">
       <h1 className="text-5xl text-center font-extrabold">Tone-deaf Mic</h1>
+      <p className="text-center">음치 마이크</p>
       <AudioProcessor />
     </div>
   );
