89c518586737e620584c13e7f5079bb8f2b1d543
최유진 <yjyj0774@gmail.com>
2024-10-14 15:22:49 +0900

    title 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import AudioProcessor from "./AudioProcessor";
 export default function Home() {
   return (
     <div className="w-96 m-auto pt-52">
-      <h1 className="text-5xl text-center">음치 마이크</h1>
+      <h1 className="text-5xl text-center font-extrabold">Tone-deaf Mic</h1>
       <AudioProcessor />
     </div>
   );
