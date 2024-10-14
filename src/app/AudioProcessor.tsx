0b8cd3a3eb6d8fd4337f5a29c11ee8750767906c
최유진 <yjyj0774@gmail.com>
2024-10-14 12:38:49 +0900

    모듈화 이전으로 되돌리기

--- src/app/AudioProcessor.tsx
+++ src/app/AudioProcessor.tsx
@@ -2,10 +2,6 @@
 
 import { useEffect, useRef, useState } from "react";
 import dynamic from "next/dynamic";
-import { initializeAudioContext } from "../modules/audioContext";
-import { handleAudioProcessing } from "../modules/audioProcessor";
-import { cleanupAudioResources } from "@/modules/cleanUp";
-import { updatePitchOffset } from "../modules/jungleProcessor";
 
 const AudioProcessor = () => {
   const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
@@ -15,20 +11,52 @@ const AudioProcessor = () => {
   const [pitchOffset, setPitchOffset] = useState(0.3);
 
   useEffect(() => {
-    initializeAudioContext(setAudioContext); // 오디오 컨텍스트 초기화
+    // 오디오 처리 초기화
+    const initAudio = async () => {
+      const context = new AudioContext(); // Web Audio API 기능에 접근하기 위해 인스턴스 생성
+      setAudioContext(context);
+    };
+
+    initAudio();
   }, []);
 
   useEffect(() => {
-    handleAudioProcessing(
-      audioContext,
-      isProcessing,
-      microphoneRef,
-      jungleRef,
-      pitchOffset
-    );
+    if (!audioContext) return;
+
+    const processAudio = async () => {
+      try {
+        const { default: Jungle } = await import("../../lib/jungle.mjs");
+        jungleRef.current = new Jungle(audioContext);
+
+        if (isProcessing) {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+          }); // 마이크 스트림 얻기
+          microphoneRef.current = audioContext.createMediaStreamSource(stream); // 얻은 마이크 스트림 입력 소스를 AudioContext에 전달
+          microphoneRef.current.connect(jungleRef.current.input); // Jungle 오디오 프로세서에 연결
+          jungleRef.current.output.connect(audioContext.destination); // 처리된 오디오를 스피커로 출력
+        } else {
+          if (microphoneRef.current) {
+            microphoneRef.current.disconnect();
+            jungleRef.current.output.disconnect();
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    processAudio();
 
     // 컴포넌트 언마운트 시 리소스 정리
-    return () => cleanupAudioResources(microphoneRef, jungleRef);
+    return () => {
+      if (microphoneRef.current) {
+        microphoneRef.current.disconnect(); // 마이크 입력 해제
+      }
+      if (jungleRef.current) {
+        jungleRef.current.output.disconnect(); // Jungle 출력 해제
+      }
+    };
   }, [audioContext, isProcessing, pitchOffset]);
 
   const handleStartStop = () => {
@@ -38,7 +66,10 @@ const AudioProcessor = () => {
   const handlePitchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newPitchOffset = parseFloat(e.target.value); // 입력받은 피치 값을 숫자로 변환
     setPitchOffset(newPitchOffset);
-    updatePitchOffset(jungleRef, newPitchOffset); // 피치 오프셋 업데이트
+
+    if (jungleRef.current) {
+      jungleRef.current.setPitchOffset(newPitchOffset); // Jungle 인스턴스의 피치 오프셋 업데이트
+    }
   };
 
   return (
