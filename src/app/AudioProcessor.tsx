21e0345a0aa9f07a5f8416ec2d1ed116ec648dbd
최유진 <yjyj0774@gmail.com>
2024-10-17 14:05:44 +0900

    디자인 수정

--- src/app/AudioProcessor.tsx
+++ src/app/AudioProcessor.tsx
@@ -102,7 +102,7 @@ const AudioProcessor = () => {
   return (
     <>
       <div>
-        <label className="relative inline-flex items-center cursor-pointer mt-16 mx-auto w-96">
+        <label className="relative inline-flex items-center cursor-pointer mt-16 mx-auto w-60">
           <input
             type="checkbox"
             checked={isProcessing} // 체크박스 상태와 isProcessing 동기화
@@ -110,7 +110,7 @@ const AudioProcessor = () => {
             placeholder="onOff"
             className="sr-only peer"
           />
-          <div className="w-14 h-8 bg-gray-200 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:absolute after:top-[3.5px] after:left-[167.5px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all peer-checked:bg-black hover:peer-checked:bg-stone-700 mx-auto"></div>
+          <div className="w-14 h-8 bg-gray-200 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:absolute after:top-[3.5px] after:left-[95.5px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all peer-checked:bg-black hover:peer-checked:bg-stone-700 mx-auto"></div>
         </label>
         <br />
         <div className="flex justify-between pt-16">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import AudioProcessor from "./AudioProcessor";
 
 export default function Home() {
   return (
-    <div className="w-96 m-auto pt-52">
-      <h1 className="text-5xl text-center font-extrabold">Tone-deaf Mic</h1>
+    <div className="w-60 m-auto pt-52">
+      <h1 className="text-3xl text-center font-extrabold">Tone-deaf Mic</h1>
       <p className="text-center">음치 마이크</p>
       <AudioProcessor />
     </div>
